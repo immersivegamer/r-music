75673b9e7ceedec9acd83e050cec6d6cd593c82d
Henry Nitz <henry.c.nitz@gmail.com>
2017-11-12 13:09:32 -0600

    remove extra comments from youtube ext. examples.

--- r-music/Program.cs
+++ r-music/Program.cs
@@ -163,41 +163,15 @@ namespace r_listentothis
 
         private static void DownloadYTA(string url, string path)
         {
-            /*
-             * Get the available video formats.
-             * We'll work with them in the video and audio download examples.
-             */
             IEnumerable<VideoInfo> videoInfos = DownloadUrlResolver.GetDownloadUrls(url);
-
-            /*
-             * Select the first .mp4 video with 360p resolution
-             */
             VideoInfo video = videoInfos
                 .First(info => info.VideoType == VideoType.Mp4 && info.Resolution == 360);
 
-            /*
-             * If the video has a decrypted signature, decipher it
-             */
             if (video.RequiresDecryption)
             {
                 DownloadUrlResolver.DecryptDownloadUrl(video);
             }
-
-            /*
-             * Create the video downloader.
-             * The first argument is the video to download.
-             * The second argument is the path to save the video file.
-             */
-            //            string outputFilePath = System.IO.Path.Combine(path, video.Title + video.VideoExtension);
             var videoDownloader = new VideoDownloader(video, path);
-
-            // Register the ProgressChanged event and print the current progress
-            //videoDownloader.DownloadProgressChanged += (sender, args) => Console.WriteLine(args.ProgressPercentage);
-
-            /*
-             * Execute the video downloader.
-             * For GUI applications note, that this method runs synchronously.
-             */
             videoDownloader.Execute();
         }
 
